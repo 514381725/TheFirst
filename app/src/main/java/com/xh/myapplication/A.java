6bbe186ce5ba3b8102653bdf885d6b8eeeae4814
lyk <514381725@qq.com>
2017-03-08 13:56:16 +0800

    tijiao

---
+++ app/src/main/java/com/xh/myapplication/A.java
@@ -0,0 +1,39 @@
+package com.xh.myapplication;
+
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+public class A {
+    public void m() {
+        System.out.print("1");
+    }
+    public static int m2(){
+        System.out.print("2");
+        return 2;
+    }
+    public  int m3(){
+        System.out.print("3");
+        return 3;
+    }
+}
+
+ class B extends A{
+    public void m4() {
+        System.out.print("4");
+    }
+    public static int m2(){
+        System.out.print("5");
+        return 5;
+    }
+    public  int m3(){
+        System.out.print("6");
+        return 6;
+    }
+     public static void main(String[] args){
+         System.out.print("7");
+        // A a=new B();
+     }
+}
+
+
+
---
+++ app/src/main/java/com/xh/myapplication/MainActivity.java
@@ -0,0 +1,12 @@
+package com.xh.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
